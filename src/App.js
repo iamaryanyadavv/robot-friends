3192522fee5b7d8590832f85e18308825b58dffb
iamaryanyadavv <aryanyadav1601@gmail.com>
2022-05-30 15:22:36 +0530

    Added scroll component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import CardList from './CardList';
 import Searchbox from './Searchbox.js';
-import { robots } from './robots';
 import './App.css';
+import Scroll from './Scroll.js';
 class App extends React.Component {
     constructor(){
         super()
         this.state= {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
-
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(Response=>{
+            return Response.json();
+        })
+        .then(users=>{
+            this.setState({robots: users});
+        })
+        
+    }
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
     render () {
         const filteredRobots=this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            return (robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()) ||
+                    robots.username.toLowerCase().includes(this.state.searchfield.toLowerCase)
+            )
         })
         return(
             <div className='tc'>
                 <h1 className='f1'>RobotFriends</h1>
                 <Searchbox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/> 
+                <Scroll>
+                    <CardList robots={filteredRobots}/> 
+                </Scroll>   
             </div>
         )
         }
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Searchbox = ({searchfield, searchChange}) => {
     return(
-        <div className='pa2'>
+        <div className='searchbar'>
             <input
                 className='search' 
                 type='search' 
